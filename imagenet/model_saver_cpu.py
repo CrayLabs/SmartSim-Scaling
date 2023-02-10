e7d9a3ab8e12bd94e47b5090afcd2277504362a0
Ale Rigazzi <al.rigazzi@hpe.com>
2023-02-10 05:35:35 -0600

    Fix wrong model saver for CPU

--- imagenet/model_saver_cpu.py
+++ imagenet/model_saver_cpu.py
@@ -1,13 +1,9 @@
 import torchvision.models as models
 import torch
-print("USING CUDA", torch.cuda.is_available())
 
 model = models.resnet50(pretrained=True)
-model.cuda()
-model.to(torch.device("cuda"))
 model.eval()
 
-batch = torch.randn((1, 3, 224, 224)).cuda()
-batch.to(torch.device("cuda"))
+batch = torch.randn((1, 3, 224, 224))
 traced_model = torch.jit.trace(model, batch)
 torch.jit.save(traced_model, 'resnet50.pt')
