642d2e8329efd846b7d20873220f7c9ce10a456c
Ale Rigazzi <al.rigazzi@hpe.com>
2023-02-10 10:55:27 -0600

    Revert testing tweaks to script

--- batch_scripts/run_standard_slurm.sh
+++ batch_scripts/run_standard_slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH -N 2
+#SBATCH -N 60
 #SBATCH --exclusive
 #SBATCH -t 10:00:00
 
 cd ..
 module load slurm
-python driver.py inference_standard --client_nodes=[2] \
-                                    --db_nodes=[1] --db_tpq=[1] \
-                                    --db_cpus=[8] --batch_args='{"C":"P100", "exclusive": None}'
+python driver.py inference_standard --client_nodes=[20,40,60] \
+                                    --db_nodes=[4,8,16] --db_tpq=[1,2,4] \
+                                    --db_cpus=[8,16] --batch_args='{"C":"P100", "exclusive": None}'
