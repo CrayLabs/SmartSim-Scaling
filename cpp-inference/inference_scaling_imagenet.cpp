0347cbea76d0b52a556b662892685f1bbe5e00ec
Al Rigazzi <al.rigazzi@hpe.com>
2023-02-11 01:10:03 +0100

    Inference scaling fix (#18)
    Small edits to fix inference tests for colocated and clustered DBs, with CPU and GPU as device.
    
    [ committed by @al-rigazzi @ashao @mellis13 ]
    [ reviewed by @billschereriii ]

--- cpp-inference/inference_scaling_imagenet.cpp
+++ cpp-inference/inference_scaling_imagenet.cpp
@@ -96,13 +96,14 @@ void run_mnist(const std::string& model_name,
         std::cout<<"Device Key " <<device_key <<std::endl<<std::flush;
 
         if (use_multi) {
+          client.set_model_from_file_multigpu(model_key, "./resnet50.pt", "TORCH", 0, num_devices, batch_size);
+          client.set_script_from_file_multigpu(script_key, "./data_processing_script.txt", 0, num_devices);
+	      }  
+        else {
           client.set_model_from_file(model_key, "./resnet50.pt", "TORCH", device_key, batch_size);
           client.set_script_from_file(script_key, device_key, "./data_processing_script.txt");
         }
-        else {
-          client.set_model_from_file_multigpu(model_key, "./resnet50.pt", "TORCH", 0, num_devices, batch_size);
-          client.set_script_from_file_multigpu(script_key, "./data_processing_script.txt", 0, num_devices);
-	}
+        
       }
     }
   }
