85bb87d790407c9bfda4c9e2e01fd866844ca399
Amanda Richardson <amanda.richardson@hpe.com>
2023-08-09 17:58:08 -0500

    fortran code

--- fortran-inference/inference_scaling_imagenet.F90
+++ fortran-inference/inference_scaling_imagenet.F90
@@ -4,6 +4,7 @@ use utils, only : get_env_var
 use smartredis_client, only : client_type
 use smartredis_errors, only : print_last_error
 use mpi
+use, intrinsic :: iso_fortran_env, only: error_unit
 
 implicit none
 
@@ -48,7 +49,6 @@ open(newunit = timing_unit, &
     )
 
 call init_client(client, rank, use_cluster, timing_unit)
-if (should_set .and. rank == 0) call set_model(client, device_type, num_devices, batch_size)
 
 model_key = "resnet_model"
 script_key = "resnet_script"
@@ -81,54 +81,6 @@ subroutine init_client( client, rank, use_cluster, timing_unit )
     write(timing_unit,'(I0,A,G0)') rank, ',client(),', delta_t
 
 end subroutine init_client
-
-subroutine set_model(client, device_type, num_devices, batch_size)
-    type(client_type), intent(in) :: client
-    character(len=*),  intent(in) :: device_type
-    integer,           intent(in) :: num_devices
-    integer,           intent(in) :: batch_size
-
-    include "enum_fortran.inc"
-
-    integer :: i
-    integer :: return_code
-    character(len=255) :: model_filename, script_filename
-    character(len=255) :: model_key, script_key
-
-    write(model_filename,'(A,A,A)') "./resnet50.", TRIM(device_type), '.pt'
-    script_filename = "../imagenet/data_processing_script.txt"
-
-    if (num_devices > 1 .and. device_type == 'GPU') then
-        model_key = 'resnet_model'
-        script_key = 'resnet_script'
-        return_code = client%set_model_from_file_multigpu( &
-            model_key, model_filename, "TORCH", 0, num_devices, batch_size)
-        if (return_code /= SRNoError) then
-            call print_last_error()
-            stop 'Error in set_model'
-        endif
-        return_code = client%set_script_from_file_multigpu(script_key, script_filename, 0, num_devices)
-        if (return_code /= SRNoError) then
-            call print_last_error()
-            stop 'Error in set_script'
-        endif
-    else
-        do i=1,num_devices
-            model_key = 'resnet_model'
-            script_key = 'resnet_script'
-            return_code = client%set_model_from_file(model_key, model_filename, "TORCH", device_type, batch_size)
-            if (return_code /= SRNoError) then
-                call print_last_error()
-                stop 'Error in set model'
-            endif
-            return_code = client%set_script_from_file(script_key, device_type, script_filename)
-            if (return_code /= SRNoError) then
-                call print_last_error()
-                stop 'Error in set script'
-            endif
-        enddo
-    endif
-end subroutine set_model
 
 subroutine run_mnist(rank, num_devices, device_type, model_key, script_key, timing_unit)
     integer,          intent(in   ) :: rank
@@ -177,14 +129,20 @@ subroutine run_mnist(rank, num_devices, device_type, model_key, script_key, timi
     else
         return_code = client%run_script(script_key, "pre_process_3ch", [in_key], [script_out_key])
     endif
-    if (return_code/=SRNoError) stop "Error in run_script (warmup)"
+    if (return_code /= SRNoError) then
+        call print_last_error()
+        stop "Error in run_script (warmup)"
+    endif
 
     if (use_multigpu) then
         return_code = client%run_model_multigpu(model_key, [script_out_key], [out_key], rank, 0, num_devices)
     else
         return_code = client%run_model(model_key, [script_out_key], [out_key])
     endif
-    if (return_code/=SRNoError) stop "Error in run_model"
+    if (return_code /= SRNoError) then
+        call print_last_error()
+        stop "Error in run_model"
+    endif
 
     return_code = client%unpack_tensor(out_key, result, [1,1000])
     if (return_code/=SRNoError) stop "Error in put tensor"
